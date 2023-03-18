37ee501e050c567a3a691eb5f59fc927c07a29f9
tomycostantino <73667097+tomycostantino@users.noreply.github.com>
2023-03-18 20:20:33 +1100

    dataset generator more functional
    now it creates only one row per client and not as many as items were, which was the problem early on

--- invoice_creation/dataset_generator.py
+++ invoice_creation/dataset_generator.py
@@ -2,7 +2,8 @@ import csv
 import random
 from faker import Faker
 
-fake = Faker()
+# using weightening will not give any weight to anything so it will be even more reandom
+fake = Faker(use_weighting=False)
 
 # create a list of product names and descriptions
 product_list = [
@@ -27,14 +28,19 @@ for i in range(100):
     city_postcode = fake.city() + ' ' + fake.postcode()
     country = fake.country()
     reference_n = fake.random_int(min=100000, max=999999)
-    invoice_date = fake.date_between(start_date='-1y', end_date='today')
+    invoice_date = fake.date_between(start_date='-5y', end_date='today')
     invoice_n = invoice_numbers.pop()
     product_items = []
+    product_items_str = ''
     for j in range(random.randint(1, 5)):
         product = random.choice(product_list)
         product_items.append(
             {'n': j + 1, 'name': product['name'], 'delivery': 'electronic', 'qty': random.randint(1, 5),
              'price': 'AU$ ' + str(random.randint(50, 200)), 'description': product['description']})
+
+        product_items_str += f'n: {j + 1}, name: {product["name"]}, delivery: electronic, qty: {random.randint(1, 5)},' \
+                             f' price: AU$ {str(random.randint(50, 200))}, description: {product["description"]}\n'
+
     subtotal = sum([item['qty'] * int(item['price'].split('$ ')[1]) for item in product_items])
     gst = round(subtotal * 0.1, 2)
     total = round(subtotal + gst, 2)
@@ -42,27 +48,26 @@ for i in range(100):
     # add the invoice to the list
     invoices.append(
         {'full_name': full_name, 'city_postcode': city_postcode, 'country': country, 'reference_n': reference_n,
-         'invoice_date': invoice_date, 'invoice_n': invoice_n, 'product_items': product_items, 'subtotal': subtotal,
+         'invoice_date': invoice_date, 'invoice_n': invoice_n, 'product_items': product_items_str, 'subtotal': subtotal,
          'gst': gst, 'total': total})
 
 # write the invoices to a CSV file
 with open('invoices.csv', mode='w', newline='') as csv_file:
-    fieldnames = ['full_name', 'city_postcode', 'country', 'reference_n', 'invoice_date', 'invoice_n', 'product_n',
-                  'product_name', 'delivery', 'qty', 'price', 'description', 'subtotal', 'gst', 'total']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    fieldnames = ['full_name', 'city_postcode', 'country', 'reference_n', 'invoice_date', 'invoice_n',
+                  'product_items', 'subtotal', 'gst', 'total']
+    writer = csv.DictWriter(csv_file, fieldnames)
     writer.writeheader()
     # create a set to keep track of invoice numbers that have been generated_docx
     used_invoice_numbers = set()
+
     for invoice in invoices:
         # keep generating a new invoice number until it is unique
         while invoice['invoice_n'] in used_invoice_numbers:
             invoice['invoice_n'] = 'AKD-' + str(random.randint(100000000, 999999999)) + str(random.randint(10, 99))
         used_invoice_numbers.add(invoice['invoice_n'])
-        for product_item in invoice['product_items']:
-            writer.writerow({'full_name': invoice['full_name'], 'city_postcode': invoice['city_postcode'],
-                             'country': invoice['country'], 'reference_n': invoice['reference_n'],
-                             'invoice_date': invoice['invoice_date'], 'invoice_n': invoice['invoice_n'],
-                             'product_n': product_item['n'], 'product_name': product_item['name'],
-                             'delivery': product_item['delivery'], 'qty': product_item['qty'],
-                             'price': product_item['price'], 'description': product_item['description'],
-                             'subtotal': invoice['subtotal'], 'gst': invoice['gst'], 'total': invoice['total']})
+
+        writer.writerow({'full_name': invoice['full_name'], 'city_postcode': invoice['city_postcode'],
+                         'country': invoice['country'], 'reference_n': invoice['reference_n'],
+                         'invoice_date': invoice['invoice_date'], 'invoice_n': invoice['invoice_n'],
+                         'product_items': invoice['product_items'],
+                         'subtotal': invoice['subtotal'], 'gst': invoice['gst'], 'total': invoice['total']})
