a547b011550b59253bf0f22b1d63e469d985d6e5
tomycostantino <73667097+tomycostantino@users.noreply.github.com>
2023-03-30 21:58:58 +1100

    it is really messy rn, but working on a better labelling

--- labeller/data_labeller.py
+++ labeller/data_labeller.py
@@ -3,6 +3,7 @@ import csv
 import typing
 import os
 import ast
+import math
 import operator
 
 
@@ -53,22 +54,21 @@ class DataLabeller:
     def _find_y1(self, values):
         return max(values)
 
-    '''
-    When getting a value, split it into individual words as Fitz library will analyse pdf word by word
-    '''
     def _split_into_words(self, value: str):
+        '''
+        When getting a value, split it into individual words as Fitz library will analyse pdf word by word
+        '''
         # get the words one by one and return a list
         if value.__contains__('-'):
             value = value.replace('-', '- ')
         return [v for v in value.split(' ')]
 
-    '''
-    If found anything, it will return a dictionary containing the word as key and the coordinates as value
-    example: {'invoice': (x0, y0, x1, y1)}
-    '''
     def _find_coordinates(self, page, words: typing.List):
-        # it will be a dictionary with the key as the word string and the value
-        # will be the word's x0,y0,x1,y1 values
+        '''
+        If found anything, it will return a dictionary containing the word as key and the coordinates as value
+        example: {'invoice': (x0, y0, x1, y1)}
+        '''
+
         word_coordinates = {}
 
         # get all words present in pages
@@ -76,29 +76,19 @@ class DataLabeller:
 
         # go through all words and see which ones match
         for word in word_list:
-            # check list isn't empty, if it is then there is no need to keep looking
-            if words:
-                # if the word string is in the list
-                if words.__contains__(word[4]) and word not in self._already_found:
-                    word_coordinates[word[4]] = word[:4]
+            # if the word string is has not been found on the document
+            if words.__contains__(word[4]) and word not in self._already_found:
+                word_coordinates[word[4]] = word[:4]
 
-                    #  add it to the found list, but add the object so it is not re-marked again
-                    self._already_found.append(word)
-
-                    # now remove it from the list we are looping through
-                    words.remove(word[4])
-
-            # when found all words bail out
-            else:
-                break
+                #  add it to the found list, but add the object so it is not re-marked again
+                self._already_found.append(word)
 
         return word_coordinates if word_coordinates else None
 
-
-    '''
-    Find the x0, y0, x1, y1 values where the section desired is present
-    '''
     def _find_subrectangle(self, locations: dict):
+        '''
+        Find the x0, y0, x1, y1 values where the section desired is present
+        '''
         x0s = []
         y0s = []
         x1s = []
@@ -115,11 +105,11 @@ class DataLabeller:
 
         return subrectangle
 
-    '''
-    Item lists are a in a nested list [[]] and they are read as a str by the read_csv function
-    so convert it to a literal and process the words
-    '''
     def _handle_item_list(self, value):
+        '''
+        Item lists are a in a nested list [[]] and they are read as a str by the read_csv function
+        so convert it to a literal and process the words
+        '''
         new_value = ''
         value = ast.literal_eval(value)
         # for item list in value
@@ -134,16 +124,85 @@ class DataLabeller:
 
         return new_value[:-1]  # remove the whitespace from the end
 
-    '''
-    This function parses the data label to the sub rectangles where they are present
-    
-    data = {'full_name': 'tomas costantino'}
-    
-    to_return = {'full_name': (x0,y0,x1,y1),
-                 'date': (x0,y0,x1,y1)}
-    '''
-    def _find_rectangles(self, page, row: dict):
+    def _get_individual_words(self, row: dict):
+        values_string = ''
 
+        for value in row.values():
+            values_string += value
+            values_string += ' '
+        return values_string.split(' ')[:-1]  # Don't include the last whitespace at the end
+
+    def _find_cw1(self):
+        pass
+
+    def _find_cw2(self):
+        pass
+
+    def _find_edcw1(self):
+        pass
+
+    def _find_edcw2(self):
+        pass
+
+    def _calculate_distance(self, w1, w2):
+        return math.sqrt(sum([pow((w1[0] - w2[0]), 2), pow((w1[1] - w2[1]), 2),
+                              pow((w1[2] - w2[2]), 2), pow((w1[3] - w2[3]), 2)]))
+
+    def _euclidean_distance(self, target, near_words):
+        distance_to_target = {}
+
+        for near_word in near_words:
+            # only pass the coordinates to calculator
+            distance_to_target[near_word] = self._calculate_distance(target[:4], near_word[:4])
+
+        return distance_to_target
+
+    def _get_context(self, words):
+        context = {}
+        for idx, word in enumerate(words):
+            # check within ranges
+            if idx - 2 >= 0 and idx + 2 <= len(words):
+                sublist = words[idx-2:idx+3]
+                sublist.pop(idx)
+                distances = self._euclidean_distance(word, sublist)
+                values_list = [value for value in distances.values()]
+                nearest_values = sorted(values_list)
+                context[word] = nearest_values[:2]
+
+            print(context)
+
+    def _determine_label(self):
+        pass
+
+    def _label_words(self, page, row: dict):
+        # get a list of the words I want to label
+        words_to_label = self._get_individual_words(row)
+
+        # get a list of the words on the document
+        words_in_file = page.get_text('words')
+        self._get_context(words_in_file)
+
+        labelled_words = {}
+
+        for word in words_in_file:
+            label = 0
+            cw1, edcw1, cw2, edcw2 = 0, 0, 0, 0
+            labelled_words[word[:4]] = {'label': label,
+                                        'word': word[4],
+                                        'cw1': cw1,
+                                        'cw2': cw2,
+                                        'eccw1': edcw1,
+                                        'eccw2': edcw2}
+
+    def _find_rectangles(self, page, row: dict):
+        '''
+        This function parses the data label to the sub rectangles where they are present
+
+        data = {'full_name': 'tomas costantino'}
+
+        to_return = {'full_name': (x0,y0,x1,y1),
+                     'date': (x0,y0,x1,y1)}
+        '''
         rectangles = {}
 
         for key, value in row.items():
@@ -157,6 +216,7 @@ class DataLabeller:
             words = self._split_into_words(value)
             locations = self._find_coordinates(page, words)
 
+            # this means that something has been found in the pdf
             if locations is not None:
                 rectangles[key] = self._find_subrectangle(locations)
 
@@ -186,6 +246,7 @@ class DataLabeller:
     def _label_document(self):
         row = self._match_pdfname_to_row()
         for page in self._current_pdf.pages():
+            self._label_words(page, row)
             rectangles = self._find_rectangles(page, row)
             self._mark_rectangles(page, rectangles) if rectangles else None
 
