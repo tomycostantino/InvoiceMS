ebfdd39daf7bbb3fc4a1bf66ebe5598628bcaa19
tomycostantino <73667097+tomycostantino@users.noreply.github.com>
2023-04-05 20:40:26 +1000

    will move from jinja docx to jinja pdf

--- invoice_creation/invoice_template_1/template_1.py
+++ invoice_creation/invoice_template_1/template_1.py
@@ -1,5 +1,36 @@
+import csv
 import ast
-from invoice_creation.template_filler import TemplateFiller
+import os
+from pdfjinja import PdfJinja
+
+
+class TemplateFiller:
+    def __init__(self):
+        pass
+
+    def read_csv(self, file):
+        # Load the CSV data
+        with open(file, 'r') as f:
+            reader = csv.DictReader(f)
+            return [row for row in reader]
+
+    def fill_template(self, file, rows):
+        # Loop through the data and generate the invoices
+        # Load the template file
+        pdfjinja = PdfJinja(file)
+
+        # create the folder if it doesn't already exist
+        if not os.path.exists('generated_pdf'):
+            os.makedirs('generated_pdf')
+
+        for i, row in enumerate(rows):
+            # the data column is the list of items, and usually comes as a str
+            if type(row['data']) == str:
+                row['data'] = ast.literal_eval(row['data'])
+
+            rendered_pdf = pdfjinja(row)
+            with open(f"generated_pdf/{i + 1}.pdf", "wb") as output:
+                output.write(rendered_pdf.read())
 
 
 def main():
@@ -7,7 +38,10 @@ def main():
 
     rows = filler.read_csv(input('Insert csv filename with extension included: '))
 
-    filler.fill_template('1.docx', rows)
+    for row in rows:
+        if type(row['data']) == str:
+            row['data'] = ast.literal_eval(row['data'])
+    filler.fill_template('1.pdf', rows)
 
 
 if __name__ == '__main__':
