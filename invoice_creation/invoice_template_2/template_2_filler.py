e68816a8d9207a377db97a8ba29250a44c73e303
tomycostantino <73667097+tomycostantino@users.noreply.github.com>
2023-04-09 21:00:38 +1000

    invoice 2 dataset and template done

---
+++ invoice_creation/invoice_template_2/template_2_filler.py
@@ -0,0 +1,27 @@
+import csv
+import json
+from docxtpl import DocxTemplate, RichText, InlineImage
+import os
+
+# Read the CSV file and load the data
+with open('random_data.csv', 'r', newline='') as csvfile:
+    csv_reader = csv.DictReader(csvfile)
+    rows = [row for row in csv_reader]
+
+
+# create the folder if it doesn't already exist
+if not os.path.exists('generated_docx'):
+    os.makedirs('generated_docx')
+
+template = DocxTemplate('2.docx')
+
+
+# Render the invoices using the docx template
+for i, row in enumerate(rows):
+    row['invoice_n'] = row['invoice_n'][:10]
+    row["data"] = json.loads(row["data"])  # Convert the JSON string to a list
+    # Render the invoice using the data
+    template.render(row)
+
+    # Save the invoice to a docx file
+    template.save(f'generated_docx/{i + 1}.docx')
